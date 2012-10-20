ced0e4c3d282185d6fcf64711710262b06cfec57
forhappy <haipingf@gmail.com>
2012-10-20 18:44:40 +0800

    add uthash.

---
+++ src/server/thread.c
@@ -0,0 +1,352 @@
+/*
+ * =============================================================================
+ *
+ *       Filename:  thread.c
+ *
+ *    Description:  thread utility.
+ *
+ *        Created:  10/18/2012 12:54:23 PM
+ *
+ *         Author:  Fu Haiping (forhappy), haipingf@gmail.com
+ *        Company:  ICT ( Institute Of Computing Technology, CAS )
+ *
+ * =============================================================================
+ */
+
+#include <assert.h>
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+
+#include "thread.h"
+#include "lldbserver.h"
+
+/* connection lock around accepting new connections */
+pthread_mutex_t conn_lock = PTHREAD_MUTEX_INITIALIZER;
+
+#if !defined(HAVE_GCC_ATOMICS)
+pthread_mutex_t atomics_mutex = PTHREAD_MUTEX_INITIALIZER;
+#endif
+
+static cqi_pool_t *item_pool;
+
+static MASTER_THREAD dispatcher_thread;
+
+/*
+ * Each libevent instance has a wakeup pipe, which other threads
+ * can use to signal that they've put a new connection on its queue.
+ */
+static SLAVE_THREAD *threads;
+
+/*
+ * number of worker threads that have finished setting themselves up.
+ */
+static int init_count = 0;
+static pthread_mutex_t init_lock;
+static pthread_cond_t init_cond;
+
+
+static void thread_libevent_process(int fd, short which, void *arg);
+
+unsigned short refcount_incr(unsigned short *refcount) {
+#ifdef HAVE_GCC_ATOMICS
+    return __sync_add_and_fetch(refcount, 1);
+#else
+    unsigned short res;
+    mutex_lock(&atomics_mutex);
+    (*refcount)++;
+    res = *refcount;
+    mutex_unlock(&atomics_mutex);
+    return res;
+#endif
+}
+
+unsigned short refcount_decr(unsigned short *refcount) {
+#ifdef HAVE_GCC_ATOMICS
+    return __sync_sub_and_fetch(refcount, 1);
+#else
+    unsigned short res;
+    mutex_lock(&atomics_mutex);
+    (*refcount)--;
+    res = *refcount;
+    mutex_unlock(&atomics_mutex);
+    return res;
+#endif
+}
+static void lldb_wait_for_thread_registration(int nthreads) {
+    while (init_count < nthreads) {
+        pthread_cond_wait(&init_cond, &init_lock);
+    }
+}
+
+static void lldb_register_thread_initialized(void) {
+    pthread_mutex_lock(&init_lock);
+    init_count++;
+    pthread_cond_signal(&init_cond);
+    pthread_mutex_unlock(&init_lock);
+}
+
+/*
+ * initializes a connection queue.
+ */
+static void
+connection_queue_init(connection_queue_t *queue)
+{
+    pthread_mutex_init(&(queue->lock), NULL);
+    pthread_cond_init(&(queue->cond), NULL);
+    queue->head = NULL;
+    queue->tail = NULL;
+}
+
+/*
+ * looks for an item on a connection queue, but doesn't block if there isn't
+ * one.
+ * returns the item, or NULL if no item is available
+ */
+static connection_queue_item_t *
+connection_queue_pop(connection_queue_t *queue)
+{
+    connection_queue_item_t *item;
+
+    pthread_mutex_lock(&(queue->lock));
+    item = queue->head;
+    if (NULL != item) {
+        queue->head = item->next;
+        if (NULL == queue->head)
+            queue->tail = NULL;
+    }
+    pthread_mutex_unlock(&queue->lock);
+
+    return item;
+}
+
+/*
+ * adds an item to a connection queue.
+ */
+static void
+connection_queue_push(connection_queue_t *queue, connection_queue_item_t *item)
+{
+    item->next = NULL;
+
+    pthread_mutex_lock(&queue->lock);
+    if (NULL == cq->tail)
+        cq->head = item;
+    else
+        cq->tail->next = item;
+    cq->tail = item;
+    pthread_cond_signal(&(queue->cond));
+    pthread_mutex_unlock(&(queue->lock));
+}
+
+/*
+ * Creates a worker thread.
+ */
+static void create_worker(void *(*func)(void *), void *arg) {
+    pthread_t       thread;
+    pthread_attr_t  attr;
+    int             ret;
+
+    pthread_attr_init(&attr);
+
+    if ((ret = pthread_create(&thread, &attr, func, arg)) != 0) {
+        fprintf(stderr, "Can't create thread: %s\n",
+                strerror(ret));
+        exit(1);
+    }
+}
+
+/*
+ * Sets whether or not we accept new connections.
+ */
+void accept_new_conns(const bool do_accept) {
+    pthread_mutex_lock(&conn_lock);
+    do_accept_new_conns(do_accept);
+    pthread_mutex_unlock(&conn_lock);
+}
+/****************************** LIBEVENT THREADS *****************************/
+
+/*
+ * Set up a thread's information.
+ */
+static void setup_thread(LIBEVENT_THREAD *me) {
+    me->base = event_init();
+    if (! me->base) {
+        fprintf(stderr, "Can't allocate event base\n");
+        exit(1);
+    }
+
+    /* Listen for notifications from other threads */
+    event_set(&me->notify_event, me->notify_receive_fd,
+              EV_READ | EV_PERSIST, thread_libevent_process, me);
+    event_base_set(me->base, &me->notify_event);
+
+    if (event_add(&me->notify_event, 0) == -1) {
+        fprintf(stderr, "Can't monitor libevent notify pipe\n");
+        exit(1);
+    }
+
+    me->new_conn_queue = malloc(sizeof(struct conn_queue));
+    if (me->new_conn_queue == NULL) {
+        perror("Failed to allocate memory for connection queue");
+        exit(EXIT_FAILURE);
+    }
+    cq_init(me->new_conn_queue);
+
+    if (pthread_mutex_init(&me->stats.mutex, NULL) != 0) {
+        perror("Failed to initialize mutex");
+        exit(EXIT_FAILURE);
+    }
+
+    me->suffix_cache = cache_create("suffix", SUFFIX_SIZE, sizeof(char*),
+                                    NULL, NULL);
+    if (me->suffix_cache == NULL) {
+        fprintf(stderr, "Failed to create suffix cache\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+/*
+ * Worker thread: main event loop
+ */
+static void *lldb_worker_libevent_callback(void *arg) {
+    LIBEVENT_THREAD *me = arg;
+
+    /* Any per-thread setup can happen here; thread_init() will block until
+     * all threads have finished initializing.
+     */
+    lldb_register_thread_initialized();
+    event_base_loop(me->base, 0);
+    return NULL;
+}
+
+
+/*
+ * Processes an incoming "handle a new connection" item. This is called when
+ * input arrives on the libevent wakeup pipe.
+ */
+static void thread_libevent_process(int fd, short which, void *arg) {
+    LIBEVENT_THREAD *me = arg;
+    CQ_ITEM *item;
+    char buf[1];
+
+    if (read(fd, buf, 1) != 1)
+        if (settings.verbose > 0)
+            fprintf(stderr, "Can't read from libevent pipe\n");
+
+    switch (buf[0]) {
+    case 'c':
+    item = cq_pop(me->new_conn_queue);
+
+    if (NULL != item) {
+        conn *c = conn_new(item->sfd, item->init_state, item->event_flags,
+                           item->read_buffer_size, item->transport, me->base);
+        if (c == NULL) {
+            if (IS_UDP(item->transport)) {
+                fprintf(stderr, "Can't listen for events on UDP socket\n");
+                exit(1);
+            } else {
+                if (settings.verbose > 0) {
+                    fprintf(stderr, "Can't listen for events on fd %d\n",
+                        item->sfd);
+                }
+                close(item->sfd);
+            }
+        } else {
+            c->thread = me;
+        }
+        cqi_free(item);
+    }
+        break;
+    /* we were told to flip the lock type and report in */
+    case 'l':
+    me->item_lock_type = ITEM_LOCK_GRANULAR;
+    lldb_register_thread_initialized();
+        break;
+    case 'g':
+    me->item_lock_type = ITEM_LOCK_GLOBAL;
+    lldb_register_thread_initialized();
+        break;
+    }
+}
+
+/* Which thread we assigned a connection to most recently. */
+static int last_thread = -1;
+
+/*
+ * dispatches a new connection to another thread. This is only ever called
+ * from the main thread, either during initialization (for UDP) or because
+ * of an incoming connection.
+ */
+void
+lldb_dispatch_conn_new(int sfd, enum conn_states init_state, int event_flags,
+                       int read_buffer_size, enum network_transport transport) {
+    CQ_ITEM *item = cqi_new();
+    char buf[1];
+    int tid = (last_thread + 1) % settings.num_threads;
+
+    LIBEVENT_THREAD *thread = threads + tid;
+
+    last_thread = tid;
+
+    item->sfd = sfd;
+    item->init_state = init_state;
+    item->event_flags = event_flags;
+    item->read_buffer_size = read_buffer_size;
+    item->transport = transport;
+
+    cq_push(thread->new_conn_queue, item);
+
+    buf[0] = 'c';
+    if (write(thread->notify_send_fd, buf, 1) != 1) {
+        perror("Writing to thread notify pipe");
+    }
+}
+
+/*
+ * returns true if this is the thread that listens for new TCP connections.
+ */
+int lldb_is_listen_thread() {
+    return pthread_self() == dispatcher_thread.thread_id;
+}
+
+void lldb_initialize_threads(int nthreads, struct event_base *main_base)
+{
+    int         i;
+    int         power;
+
+    pthread_mutex_init(&init_lock, NULL);
+    pthread_cond_init(&init_cond, NULL);
+
+    pthread_mutex_init(&cqi_freelist_lock, NULL);
+    cqi_freelist = NULL;
+    
+    threads = calloc(nthreads, sizeof(LIBEVENT_THREAD));
+    if (threads == NULL) {
+        exit(1);
+    }
+
+    dispatcher_thread.base = main_base;
+    dispatcher_thread.thread_id = pthread_self();
+
+    for (i = 0; i < nthreads; i++) {
+        int fds[2];
+        if (pipe(fds)) exit(1);
+
+        threads[i].notify_receive_fd = fds[0];
+        threads[i].notify_send_fd = fds[1];
+
+        setup_thread(&threads[i]);
+    }
+
+    /* Create threads after we've done all the libevent setup. */
+    for (i = 0; i < nthreads; i++) {
+        lldb_create_worker(worker_libevent, &threads[i]);
+    }
+
+    /* Wait for all the threads to set themselves up before returning. */
+    pthread_mutex_lock(&init_lock);
+    lldb_wait_for_thread_registration(nthreads);
+    pthread_mutex_unlock(&init_lock);
+}
---
+++ src/server/thread.h
@@ -0,0 +1,22 @@
+/*
+ * =============================================================================
+ *
+ *       Filename:  thread.h
+ *
+ *    Description:  thread utility.
+ *
+ *        Created:  10/18/2012 02:34:22 PM
+ *
+ *         Author:  Fu Haiping (forhappy), haipingf@gmail.com
+ *        Company:  ICT ( Institute Of Computing Technology, CAS )
+ *
+ * =============================================================================
+ */
+
+#ifndef LLDB_THREAD_H
+#define LLDB_THREAD_H
+
+#include "lldbserver.h"
+
+
+#endif // LLDB_THREAD_H

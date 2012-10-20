8a0e3d0889f6bb26a217acabadad8492aed60431
forhappy <haipingf@gmail.com>
2012-10-20 18:43:48 +0800

    add connection handler.

---
+++ src/server/connection.c
@@ -0,0 +1,16 @@
+/*
+ * =============================================================================
+ *
+ *       Filename:  connection.c
+ *
+ *    Description:  connection implementation.
+ *
+ *        Created:  10/20/2012 03:16:45 PM
+ *
+ *         Author:  Fu Haiping (forhappy), haipingf@gmail.com
+ *        Company:  ICT ( Institute Of Computing Technology, CAS )
+ *
+ * =============================================================================
+ */
+
+#include "connection.h"
---
+++ src/server/connection.h
@@ -0,0 +1,21 @@
+/*
+ * =============================================================================
+ *
+ *       Filename:  connection.h
+ *
+ *    Description:  lldbserver connection data structure.
+ *
+ *        Created:  10/20/2012 02:59:24 PM
+ *
+ *         Author:  Fu Haiping (forhappy), haipingf@gmail.com
+ *        Company:  ICT ( Institute Of Computing Technology, CAS )
+ *
+ * =============================================================================
+ */
+
+#ifndef LLDB_CONNECTION_H
+#define LLDB_CONNECTION_H
+
+#include "lldbserver.h"
+
+#endif // LLDB_CONNECTION_H

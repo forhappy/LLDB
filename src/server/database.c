8d65fcde007e379e4b0f3d9941161ad97d955408
forhappy <haipingf@gmail.com>
2012-10-20 18:44:57 +0800

    add database handler.

---
+++ src/server/database.c
@@ -0,0 +1,16 @@
+/*
+ * =============================================================================
+ *
+ *       Filename:  database.c
+ *
+ *    Description:  database handler.
+ *
+ *        Created:  10/20/2012 03:16:53 PM
+ *
+ *         Author:  Fu Haiping (forhappy), haipingf@gmail.com
+ *        Company:  ICT ( Institute Of Computing Technology, CAS )
+ *
+ * =============================================================================
+ */
+
+#include "database.h"
---
+++ src/server/database.h
@@ -0,0 +1,21 @@
+/*
+ * =============================================================================
+ *
+ *       Filename:  database.h
+ *
+ *    Description:  leveldb database handler.
+ *
+ *        Created:  10/20/2012 03:00:37 PM
+ *
+ *         Author:  Fu Haiping (forhappy), haipingf@gmail.com
+ *        Company:  ICT ( Institute Of Computing Technology, CAS )
+ *
+ * =============================================================================
+ */
+
+#ifndef LLDB_DATABASE_H
+#define LLDB_DATABASE_H
+
+
+
+#endif // LLDB_DATABASE_H
